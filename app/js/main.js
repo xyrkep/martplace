a9ff962c6ee0e62237193959e6afae051db03423
xyrkep <serge@coudeau.fr>
2020-02-16 16:02:37 +0100

    added advantages section

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,17 @@
-$(function(){
+$(function () {
 
-$('.search__inner-btn_view').on('click', function(){
-    $('.search__inner-btn_view').addClass('search__inner-btn_active');
-    $('.search__inner-btn_popular').addClass('search__inner-btn');
-    $('.search__inner-btn_view').removeClass('search__inner-btn');
-    $('.search__inner-btn_popular').removeClass('search__inner-btn_active');
-});
+    $('.search__inner-btn_view').on('click', function () {
+        $('.search__inner-btn_view').addClass('search__inner-btn_active');
+        $('.search__inner-btn_popular').addClass('search__inner-btn');
+        $('.search__inner-btn_view').removeClass('search__inner-btn');
+        $('.search__inner-btn_popular').removeClass('search__inner-btn_active');
+    });
 
-$('.search__inner-btn_popular').on('click', function(){
-    $('.search__inner-btn_popular').addClass('search__inner-btn_active');
-    $('.search__inner-btn_view').addClass('search__inner-btn');
-    $('.search__inner-btn_popular').removeClass('search__inner-btn');
-    $('.search__inner-btn_view').removeClass('search__inner-btn_active');
-});
+    $('.search__inner-btn_popular').on('click', function () {
+        $('.search__inner-btn_popular').addClass('search__inner-btn_active');
+        $('.search__inner-btn_view').addClass('search__inner-btn');
+        $('.search__inner-btn_popular').removeClass('search__inner-btn');
+        $('.search__inner-btn_view').removeClass('search__inner-btn_active');
+    });
 
 });
